8aec1335d816a2fc7c0c4975bc4fa7545de4fdff
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2019-04-24 15:01:03 +0900

    風ギミック

---
+++ Assets/Saitou/Script/IGimmickEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarProject2019.Saitou
+{
+    /// <summary>
+    /// ギミックの動作を実装
+    /// </summary>
+    public interface IGimmickEffect
+    {
+        /// <summary>
+        /// ギミックの効果発動
+        /// </summary>
+        /// <param name="_target">効果を与える相手</param>
+        void ActiveEffect(GameObject _target);
+    }
+}
