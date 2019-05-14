21f5c72cddd077099bec60f37a385a21f78e650e
Kaz_Nakajima <nkc.kazuya.nakajima@gmail.com>
2019-05-14 19:49:05 +0900

    ファイル整理、トゲの追加

---
+++ Assets/Nakajima/Script/MapObj/Niedle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Niedle : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    /// <param name="col">当たったコリジョン</param>
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // プレイヤーでないならリターン
+        var player = col.gameObject.GetComponent<Matsumoto.Character.Player>();
+        if (player == null) return;
+        
+
+    }
+}
