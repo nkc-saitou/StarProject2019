57b12e10097beb9d5c4fe55bda886a4031809ac7
Ryo-Matsumoto <nkc.ryo.matsumoto@gmail.com>
2019-05-14 14:51:48 +0900

    StageSelect[WIP] プレイヤーの移動制御変更

---
+++ Assets/Matsumoto/Scripts/System/GameData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/// <summary>
+/// データを保存しておく
+/// 参考
+/// http://kan-kikuchi.hatenablog.com/entry/Json_SaveData
+/// </summary>
+public class GameData : SingletonMonoBehaviour<GameData> {
+
+	private Dictionary<string, object> _dataList;
+
+
+	public string SavePath {
+		get; set;
+	}
+	
+	public void Save() {
+		//JsonUtility.
+	}
+
+	public void Load() {
+	
+	}
+
+	public bool SetData<T> (string key, T data) {
+		if(_dataList.ContainsKey(key)) return false;
+
+		_dataList.Add(key, data);
+		return true;
+	}
+
+	public bool GetData<T>(string key, ref T data) {
+		if(!_dataList.ContainsKey(key)) return false;
+
+		data = (T)_dataList[key];
+		return true;
+	}
+}
