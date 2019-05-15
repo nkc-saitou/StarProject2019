0d8c88cb44a59ca56cc6db3698e9fb3801eb3611
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2019-05-15 13:43:23 +0900

    風ギミック調整

--- Assets/Saitou/Script/GimmickWind.cs
+++ Assets/Saitou/Script/GimmickWind.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Matsumoto.Character;
 
 namespace StarProject2019.Saitou
 {
@@ -16,11 +17,19 @@ namespace StarProject2019.Saitou
         [SerializeField] int windPower;
         GameObject _target;
 
+        Player _player;
+
+        void Start()
+        {
+            _player = FindObjectOfType<Player>();
+        }
+
         /// <summary>
         /// 風のギミック動作
         /// </summary>
         public void ActiveEffect()
         {
+            if (_player.State != PlayerState.Circle) return;
             // 対象にRigidbodyが無ければギミックの効果を発動させない
             Rigidbody2D _rg = _target.GetComponent<Rigidbody2D>();
             if (_rg == null) return;
