dc9e2d5920d47f9141b03a65a5c3d552f955f759
Ryo-Matsumoto <nkc.ryo.matsumoto@gmail.com>
2019-05-16 16:58:19 +0900

    ゲームオーバー時のリトライ機能を追加

--- Assets/Matsumoto/Scripts/Stages/StageController.cs
+++ Assets/Matsumoto/Scripts/Stages/StageController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using System;
 
+public enum GameState {
+	StartUp,
+	Playing,
+	GameClear,
+	GameOver,
+}
+
 public class StageController : MonoBehaviour {
 
 	public event Action<StageController> OnGameStart;
 	public event Action<StageController> OnGameClear;
 	public event Action<StageController> OnGameOver;
+
+	public GameState State {
+		get; private set;
+	} = GameState.StartUp;
 
 	private void Awake() {
 		// ステージ生成
@@ -19,24 +30,36 @@ public class StageController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
 		GameStart();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if(State == GameState.GameOver) {
+			//リトライ
+			if(Input.GetButtonDown("Attack")) {
+				SceneMover.MoveScene("GameScene");
+			}
+		}
+
 	}
 
 	public void GameStart() {
 
 		Debug.Log("GameStart!");
+
+		State = GameState.Playing;
+
 		OnGameStart?.Invoke(this);
 	}
 
 	public void GameClear() {
 
 		Debug.Log("GameClear!");
+
+		State = GameState.GameClear;
+
 		OnGameClear?.Invoke(this);
 	}
 
@@ -44,5 +67,12 @@ public class StageController : MonoBehaviour {
 
 		Debug.Log("GameOver!");
 		OnGameOver?.Invoke(this);
+
+		StartCoroutine(GameOverWait());
+	}
+
+	IEnumerator GameOverWait() {
+		yield return new WaitForSeconds(1);
+		State = GameState.GameOver;
 	}
 }
