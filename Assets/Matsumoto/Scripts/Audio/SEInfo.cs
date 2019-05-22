10218a3abdfd7bcad3ab3eca2a260cd8f179f136
Ryo-Matsumoto <nkc.ryo.matsumoto@gmail.com>
2019-05-22 13:59:08 +0900

    音声関連を追加

---
+++ Assets/Matsumoto/Scripts/Audio/SEInfo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace DDD.Matsumoto.Audio {
+
+	/// <summary>
+	/// 再生されているSEの情報
+	/// </summary>
+	public class SEInfo {
+
+		public int Index;
+		public float CurTime;
+		public float Volume;
+
+		public SEInfo(int index, float curTime, float volume) {
+			Index = index;
+			CurTime = curTime;
+			Volume = volume;
+		}
+	}
+}
