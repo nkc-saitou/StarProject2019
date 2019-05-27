e4e6d560404fc77b8cdc9138e3cc416707340220
Kaz_Nakajima <nkc.kazuya.nakajima@gmail.com>
2019-05-27 17:10:23 +0900

    当たり判定の修正、レーザーのインターバル処理の追加

--- Assets/Nakajima/Script/Enemy/PatrolEnemy.cs
+++ Assets/Nakajima/Script/Enemy/PatrolEnemy.cs
@@ -33,6 +33,7 @@ public class PatrolEnemy : EnemyBase, IEnemy
     void Start()
     {
         lineRen = GetComponent<LineRenderer>();
+        lineRen.enabled = false;
         target = GameObject.Find("Player");
         currentPos = transform.position;
         originPos = transform.position;
@@ -94,19 +95,30 @@ public class PatrolEnemy : EnemyBase, IEnemy
     /// </summary>
     public void Action()
     {
+        // プレイヤー判定用レイヤー
         int playerLayer = LayerMask.GetMask("Player", "PlayerFollower");
+        // 地面判定用レイヤー
         int groundLayer = LayerMask.GetMask("Player", "PlayerFollower", "Ignore Raycast");
 
-        RaycastHit2D groundHit = Physics2D.Raycast(transform.position, -transform.up, 500.0f, ~groundLayer);
+        // LineRendererをアクティブにする
+        if (lineRen.enabled == false) {
+            lineRen.enabled = true;
+            StartCoroutine(IntervalAction(2.0f));
+        }
 
+        // 地面判定
+        RaycastHit2D groundHit = Physics2D.Raycast(transform.position, -transform.up, 500.0f, ~groundLayer);
+        // 地面の地点取得
         endPos = groundHit.point;
 
+        // Lineの位置を定義
         lineRen.SetPosition(0, startObj.transform.position);
         lineRen.SetPosition(1, endPos);
-
+        // Lineの長さを定義
         lineRen.startWidth = 0.2f;
         lineRen.endWidth = 0.2f;
 
+        // プレイヤー判定
         RaycastHit2D playerHit = Physics2D.Raycast(transform.position, -transform.up, 500.0f, playerLayer);
         if(playerHit.collider != null) {
             var player = playerHit.collider.gameObject.GetComponent<Matsumoto.Character.Player>();
@@ -123,7 +135,25 @@ public class PatrolEnemy : EnemyBase, IEnemy
     {
         yield return new WaitForSeconds(_interval);
 
-        canAction = true;
+        if (canAction == true) {
+            canAction = false;
+            StartCoroutine(IntervalAction(1.0f));
+        }
+        else canAction = true;
+
+        if (lineRen.enabled == true) lineRen.enabled = false;
+    }
+
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    /// <param name="col">当たったコリジョン</param>
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        var player = col.gameObject.GetComponent<Matsumoto.Character.Player>();
+        if (player == null) return;
+
+        player.ApplyDamage(gameObject, DamageType.Enemy);
     }
 
     /// <summary>
