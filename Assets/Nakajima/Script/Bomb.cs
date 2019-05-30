e09c480cb3627a8b386b2a74569d8295ae0de161
Kaz_Nakajima <nkc.kazuya.nakajima@gmail.com>
2019-05-30 19:26:37 +0900

    ステージの見た目修正

--- Assets/Nakajima/Script/Bomb.cs
+++ Assets/Nakajima/Script/Bomb.cs
@@ -13,15 +13,15 @@ public class Bomb : MonoBehaviour
 
 	// Use this for initialization
 	void Start () {
+        // ターゲットの設定
         targetObj = FindObjectOfType<Matsumoto.Character.Player>().gameObject;
+        var player = targetObj.GetComponent<Matsumoto.Character.Player>();
 
+        // 距離を測る
         float distance =  CheckDistance(targetObj.transform.position);
 
         if(distance <= explosionRange) {
-            var player = targetObj.GetComponent<Matsumoto.Character.Player>();
-            if (player == null) return;
-
-            player.ApplyDamage(gameObject, DamageType.Enemy);
+            player.ApplyDamage(gameObject, DamageType.Gimmick);
         }
 	}
 	
